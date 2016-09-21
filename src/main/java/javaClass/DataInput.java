62608563ffd075e4044313aac746a46c5c9f7ac5
ustenko <free.ukrainev@gmail.com>
2016-09-21 16:54:56 +0300

    21-09-2016 16:54

---
+++ src/main/java/javaClass/DataInput.java
@@ -0,0 +1,21 @@
+package javaClass;
+
+/**
+ * Created by ustenko on 21.09.16.
+ */
+public class DataInput {
+
+    public String title = "";
+    public String auvtor = "";
+    public int page = 0;
+
+    public String dataInput(String title, String auvtor, int page){
+
+
+
+
+        return title;
+    }
+
+
+}
--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -1,33 +1,85 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Calendar;
 
 /**
  * Created by ustenko on 20.09.16.
  */
-public class MainServlet {
+public class MainServlet extends HttpServlet {
 
 
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    //В строке объявляется и иннициализируктся строковая переменная, которая задает путь к файлу со стилями
+    private String styleSheet = "<link type=\"text/css\" rel=\"stylesheet\" href=\"style.css\">";
+
+
+    @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
                 throws ServletException, IOException {
 
-            PrintWriter out = resp.getWriter();
+            PrintWriter out = response.getWriter();
             out.print("<h1>Hello Servlet</h1>");
 
-            super.doGet(req, resp);
+            super.doGet(request, response);
         }
 
         @Override
-        protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        protected void doPost(HttpServletRequest request, HttpServletResponse response, boolean isPost)
                 throws ServletException, IOException {
 
-            super.doPost(req, resp);
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+
+            try {
+                out.println("<html><head>");
+                out.println(styleSheet);
+                out.println("</head><body>");
+                out.println("<table width=\"95%\" align=\"center\"><tr><td>");
+
+                // Установим
+                String title = "";
+                String auvtor = "";
+                //String pages = "";
+                int page = 0;
+
+                if (isPost) {
+                    title = request.getParameter("title");
+                    if (null == title) title = "";
+                    auvtor = request.getParameter("auvtor");
+                    if (null == auvtor) auvtor = "";
+                    //pages = request.getParameter("page");
+                    //if (null == pages) pages = "";
+                    //if ((!pages.isEmpty())  //Проверяет, является ли строка пустой
+                    //        && (pages.matches("[0-9]+")))
+                    //    page = Integer.parseInt(pages);
+                    if (0 == page)
+                        page = page;//??????????????
+
+                }
+
+
+
+
+       //     } catch (ClassNotFoundException e) {
+       //        out.println(printError("Ошибка при работе с базой данных<br>" + e.toString()));
+         //   } catch (SQLException e) {
+        //        out.println(printError("Ошибка при работе с базой данных<br>" + e.toString()));
+            } catch (NumberFormatException e) {
+                out.println(printError("Ошибка при обработке данных<br>" + e.toString()));
+
+            }
+            super.doPost(request, response);
         }
+
+    private boolean printError(String s) {
+        return false;
+    }
 
 
 }
